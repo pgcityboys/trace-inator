a85ec572b0cb3aa3dd00e6443cb9cb175e18b7e9
lukrzak <lukrzak@wp.pl>
2024-01-10 19:29:23 +0100

    Created models

---
+++ src/api/model/step.py
@@ -0,0 +1,16 @@
+class Step:
+    def __init__(self, start_coordinates: dict, end_coordinates: dict, distance: int, duration: int, instruction: str):
+        self.start_coordinates = start_coordinates
+        self.end_coordinates = end_coordinates
+        self.distance = distance
+        self.duration = duration
+        self.instruction = instruction
+
+    def __str__(self):
+        return str({
+            "start_coordinates": self.start_coordinates,
+            "end_coordinates": self.end_coordinates,
+            "distance": self.distance,
+            "duration": self.duration,
+            "instruction": self.instruction
+        })
---
+++ src/api/model/trace.py
@@ -0,0 +1,6 @@
+from src.api.model.path import Path
+
+
+class TraceModel:
+    def __init__(self, paths: list[Path]):
+        self.paths = paths
